8946a63d1d7c6e8df72b437b784fb47c46cec0fa
rjrosag <rj.rosag@gmail.com>
2020-03-08 11:27:51 -0400

    %1

--- gui/ctrpv0.2/gitpush.sh
+++ gui/ctrpv0.2/gitpush.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 sudo git add -A 
 sudo git commit -m %1
-sudo git push https://rjrosag:Rjr261103!@github.com/rjrosag/prod.git -all 
+sudo git push https://rjrosag:Rjr261103!@github.com/rjrosag/prod.git --all 
